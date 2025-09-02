fa94fa70942f1d8dddc3b32158f2388e2122cf41
Tekfred <asagodfred101@gmail.com>
2025-09-02 10:11:31 +0000

    updated service and serviceCard

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,13 +1,12 @@
+
 import { services } from "../constants"
 
 const Services = () => {
   return (
-    <section className="max-container flex justify-center flex-wrap gap-9">
-       {services.map((service) =>(
-        <serviceCard key ={service.label}  
-          {...service}
-        />
-       ))}
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
     </section>
   )
 }
